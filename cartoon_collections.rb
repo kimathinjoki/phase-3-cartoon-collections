b24cc82bad452775c569bb36f0921ba799c29342
kimathinjoki <dfkimathinjoki@gmail.com>
2023-02-11 00:38:14 -0800

    Completed assignment

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(arr)# code an argument here
   # Your code here
+  puts arr.each.with_index(1){|b,index| puts "#{index}. #{b}"}
 end
 
-def summon_captain_planet# code an argument here
+puts roll_call_dwarves(["dof", "breef", "brom"])
+
+def summon_captain_planet(arr)# code an argument here
   # Your code here
+  arr.map{|b| "#{b.capitalize}" + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(arr) #code an argument here
   # Your code here
+  arr.any? do |b|
+    b.size>4
+  end
+
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arr)
+  # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+arr.find{ |b| cheese_types.include?(b)}
+
+
+
 end
